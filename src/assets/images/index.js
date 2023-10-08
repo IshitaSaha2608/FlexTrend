24e05c44aab2fee7e9841565f0088d78bef6c40d
Sayantan Sen <91620217+ShayWonTon@users.noreply.github.com>
2023-10-08 17:18:00 +0530

    Update index.js database

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -24,30 +24,27 @@ import cosmetic1 from "./products/newArrival/cosmetic1.webp";
 import cosmetic5 from "./products/newArrival/cosmetic5.webp";
 import cosmetic8 from "./products/newArrival/cosmetic8.webp";
 import cosmetic11 from "./products/newArrival/cosmetic11.webp";
-import cosmetic12 from "./products/newArrival/cosmetic12.webp";
+
 import cosmetic15 from "./products/newArrival/cosmetic15.webp";
 import cosmetic16 from "./products/newArrival/cosmetic16.webp";
 import cosmetic24 from "./products/newArrival/cosmetic24.webp";
 import cosmetic31 from "./products/newArrival/cosmetic31.webp";
-import cosmetic34 from "./products/newArrival/cosmetic34.webp";
 import cosmetic40 from "./products/newArrival/cosmetic40.webp";
 import shoe1 from "./products/newArrival/shoe1.webp";
 import shoe2 from "./products/newArrival/shoe2.webp";
 import shoe3 from "./products/newArrival/shoe3.webp";
-import shoe4 from "./products/newArrival/shoe4.webp";
 import shoe5 from "./products/newArrival/shoe5.webp";
 import shoe6 from "./products/newArrival/shoe6.webp";
-import shoe7 from "./products/newArrival/shoe7.webp";
 import shoe8 from "./products/newArrival/shoe8.webp";
 import shoe16 from "./products/newArrival/shoe16.webp";
 import shoe20 from "./products/newArrival/shoe20.webp";
 import shoe21 from "./products/newArrival/shoe21.webp";
-import shoe23 from "./products/newArrival/shoe23.webp";
+
 import fashion19 from "./products/newArrival/Fasion19.webp";
 import fashion20 from "./products/newArrival/Fasion20.webp";
 import fashion10 from "./products/newArrival/fashion10.webp";
 import fashion11 from "./products/newArrival/fashion11.webp";
-import fashion12 from "./products/newArrival/fashion12.webp";
+
 import fashion2 from "./products/newArrival/fashion2.webp";
 import fashion3 from "./products/newArrival/fashion3.webp";
 import fashion4 from "./products/newArrival/fashion4.webp";
@@ -57,7 +54,7 @@ import fashion8 from "./products/newArrival/fashion8.webp";
 import fashion9 from "./products/newArrival/fashion9.webp";
 import Elec20 from "./products/newArrival/Elec20.webp";
 import Elec21 from "./products/newArrival/Elec21.webp";
-import Elec22 from "./products/newArrival/Elec22.webp";
+
 import Elec25 from "./products/newArrival/Elec25.webp";
 import Elec26 from "./products/newArrival/Elec 26.webp";
 import Elec1 from "./products/newArrival/Elec1.webp";
@@ -73,7 +70,6 @@ import home25 from "./products/newArrival/home 25.webp";
 import home26 from "./products/newArrival/home 26.webp";
 import home9 from "./products/newArrival/home 9.webp";
 import jewel1 from "./products/newArrival/jewel1.webp";
-import jewel10 from "./products/newArrival/fjewel10.webp";
 import jewel11 from "./products/newArrival/jewel11.webp";
 import jewel12 from "./products/newArrival/jewel12.webp";
 import jewel13 from "./products/newArrival/jewel13.webp";
@@ -86,7 +82,6 @@ import jewel29 from "./products/newArrival/jewel29.webp";
 import jewel3 from "./products/newArrival/jewel3.webp";
 import jewel30 from "./products/newArrival/jewel30.webp";
 import jewel4 from "./products/newArrival/jewel4.webp";
-import jewel5 from "./products/newArrival/jewel5.webp";
 import jewel6 from "./products/newArrival/jewel6.webp";
 import jewel7 from "./products/newArrival/jewel7.webp";
 import jewel8 from "./products/newArrival/jewel8.webp";
@@ -112,7 +107,7 @@ import bodycare17 from "./products/bestSeller/bodycare17.webp";
 import bodycare19 from "./products/bestSeller/bodycare19.webp";
 import bodycare22 from "./products/bestSeller/bodycare22.webp";
 import bodycare25 from "./products/bestSeller/bodycare25.webp";
-import bodycare26 from "./products/bestSeller/bodycare26.webp";
+
 import bodycare27 from "./products/bestSeller/bodycare27.webp";
 import bodycare28 from "./products/bestSeller/bodycare28.webp";
 import bodycare29 from "./products/bestSeller/bodycare29.webp";
@@ -142,7 +137,7 @@ import skin5 from "./products/bestSeller/skin 5.webp";
 import skin11 from "./products/bestSeller/skin 11.webp";
 import skin17 from "./products/bestSeller/skin 17.webp";
 import skincare9 from "./products/bestSeller/skincare9.webp";
-import skincare19 from "./products/bestSeller/skincare19.webp";
+
 import skincare23 from "./products/bestSeller/skincare23.webp";
 import skincare6 from "./products/bestSeller/Skincare 6.jpeg";
 import Elec19 from "./products/bestSeller/Elec19.webp";
@@ -165,7 +160,7 @@ import jewel28 from "./products/bestSeller/jewel28.webp";
 import home3 from "./products/bestSeller/home 3.webp";
 import home4 from "./products/bestSeller/home 4.webp";
 import home5 from "./products/bestSeller/home 5.webp";
-import home6 from "./products/bestSeller/home 6.webp";
+
 import home7 from "./products/bestSeller/home 7.webp";
 import home8 from "./products/bestSeller/home 8.webp";
 import home11 from "./products/bestSeller/home 11.webp";
@@ -177,30 +172,27 @@ import spfOne from "./products/specialOffer/spfOne.webp";
 import spfTwo from "./products/specialOffer/spfTwo.webp";
 import spfThree from "./products/specialOffer/spfThree.webp";
 import spfFour from "./products/specialOffer/spfFour.webp";
-import shoe4 from "./products/specialOffer/shoe4.webp";
 import shoe5 from "./products/specialOffer/shoe5p";
 import shoe6 from "./products/specialOffer/shoe6.webp";
 import shoe8 from "./products/specialOffer/shoe8.webp";
-import cosmetic3 from "./products/specialOffer/cosmetic3.webp";
+
 import cosmetic18 from "./products/specialOffer/cosmetic18.webp";
-import cosmetic36 from "./products/specialOffer/cosmetic36.webp";
 import bodycare1 from "./products/specialOffer/bodycare1.webp";
 import bodycare15 from "./products/specialOffer/bodycare15.webp";
-import bodycare21 from "./products/specialOffer/bodycare21.webp";
+
 import bodycare23 from "./products/specialOffer/bodycare23.webp";
 import bodycare24 from "./products/specialOffer/bodycare24.webp";
 import skincare2 from "./products/specialOffer/Skincare 2.webp";
 import skincare3 from "./products/specialOffer/Skincare 3.webp";
 import jewel19 from "./products/specialOffer/jewel19.webp";
-import fashion13 from "./products/specialOffer/fashion13.webp";
 import Fasion17 from "./products/specialOffer/Fasion17.webp";
 import home2 from "./products/specialOffer/home 2.webp";
 import home16 from "./products/specialOffer/home 16.webp";
-import jewel31 from "./products/specialOffer/jewel31.webp";
+
 import Elec18 from "./products/specialOffer/Elec18.webp";
 import Elec16 from "./products/specialOffer/Elec16.webp";
 import Elec7 from "./products/specialOffer/Elec7.webp";
-import home18 from "./products/specialOffer/home 18.webp";
+
 
 
 
@@ -243,25 +235,20 @@ export {
   cosmetic16,
   cosmetic24,
   cosmetic31,
-  cosmetic34,
   cosmetic40,
   shoe1,
   shoe2,
   shoe3,
-  shoe4,
   shoe5,
   shoe6,
-  shoe7,
   shoe8,
   shoe16,
   shoe20,
   shoe21,
-  shoe23,
   fashion19,
   fashion20,
   fashion10,
   fashion11,
-  fashion12,
   fashion2,
   fashion3,
   fashion4,
@@ -271,7 +258,6 @@ export {
   fashion9,
   Elec20,
   Elec21,
-  Elec22,
   Elec25,
   Elec26,
   Elec1,
@@ -287,7 +273,6 @@ export {
   home26,
   home9,
   jewel1,
-  jewel10,
   jewel11,
   jewel12,
   jewel13,
@@ -300,7 +285,6 @@ export {
   jewel3,
   jewel30,
   jewel4,
-  jewel5,
   jewel6,
   jewel7,
   jewel8,
@@ -327,7 +311,7 @@ export {
   bodycare19,
   bodycare22,
   bodycare25,
-  bodycare26,
+  
   bodycare27,
   bodycare28,
   bodycare29,
@@ -357,7 +341,6 @@ export {
   skin11,
   skin17,
   skincare9,
-  skincare19,
   skincare23,
   skincare6,
   Elec19,
@@ -380,7 +363,6 @@ export {
   home3,
   home4,
   home5,
-  home6,
   home7,
   home8,
   home11,
@@ -393,13 +375,11 @@ export {
   spfTwo,
   spfThree,
   spfFour,
-  shoe4,
   shoe5,
   shoe6,
   shoe8,
   cosmetic3,
   cosmetic18,
-  cosmetic36,
   skincare2,
   skincare3,
   bodycare1,
